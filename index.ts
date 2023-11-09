4c3a53ee74008c3077f290b02694e64f3b0901b2
Dheerapat Tookkane <dheerapat38@gmail.com>
2023-11-09 15:14:55 +0700

    add json parse

--- index.ts
+++ index.ts
@@ -62,7 +62,7 @@ async function sendEncryptedMessageToAPI(encryptedMessage: any, apiEndpoint: str
   }
 }
 
-async function decryptMessage(encryptedMessage: string, privateKeyFilePath: string): Promise<any> {
+async function decryptMessage(encryptedMessage: string, privateKeyFilePath: string): Promise<string> {
   try {
     const msg = `-----BEGIN PGP MESSAGE-----\n\n${encryptedMessage}\n-----END PGP MESSAGE-----`
 
@@ -131,5 +131,7 @@ const apiEndpoint = 'https://sandbox-gac.sandbox.2c2p.net/gac/api/v2.1/customers
 
 decryptMessage("hQEMAz8cKuOiir56AQgAl+U/ATc/f+4Lenjf/3Oejb24D1P/vc5kM1fi2k55EOTF3UFlVkRKkRuTNrxZ1L953Bblxu7r6UcvBSLYNWFgR9pOv/zSbyGcxKb+cNNgIdg7a8E7zgrGYaKHRwf4vdBrvuffp8E5YX4bnBiZa8u76pV1gogZFG5EFQG8NxppbD9/arsFO8/siDI1SG/zrgHEBoxrCfLhf3fnH+q5KsIHWatNs0fM8Eizjgo9IjQfPDsqY41g/riGZGIWrtY4ORbNi846j99K8FjRQvRkMOzPYQrN4ttCsRMq0jzWWVvGMVmlz0cDN0mXgC7C4TfSZi6TTCcW1uBEgneWuQGhyvJSbdLAmAH8qzHNU0LMn4AYFF8kH31L92CEXGOEwsgkAumsdfJPN5ty1XoKugSHMAnWTIxbv+6K2y2/wZiFs0EmsV/ZhXyDLq/8p/GeJhluA51Kx41fe6UerMSYdG34Bvcl7BOYdUvkZLwjG+HMR+/bQR7pChnjE1nEvJsOH6BT8QmbyBGy4PQJ8GtgmlT2A0grr9Uo89vK2PsTHVNWUfUY1N7YDYGTENmXcxPBNCm+sU68yCgHIMyMSBgtriafAifgbj769SljAgNpnail2M95NcEbjOVdY9b5XWkfV+OD2LshkZB5n/tpEvL0Aj7qgZ3fXM1VTp4sdAm4PYVIprfgFu1/l31/X1jf7p78PqgrsdTu+TVZlwGv0N6u670cbrZLftLv7pX51avlzIG+Qp/Cgg7xUwn6RTlcAFpDa2O3oBjFSKIXPOowQJPm6PM7b1YBIoyPT1ipmPpRy6Oa=o65C",
   './dheeto_s9_private.asc').then(decrypt => {
-    console.log(decrypt)
-  })
+    console.log(JSON.parse(decrypt))
+  }).catch((error) => {
+    console.error(error);
+  });
