0462dcdabd13f8e3a7735727e85e628ce6a990ac
Dheerapat Tookkane <dheerapat38@gmail.com>
2023-11-09 17:00:55 +0700

    fixing aes encoder

--- aesEncrypt.ts
+++ aesEncrypt.ts
@@ -2,7 +2,7 @@ import cryptoJS from "crypto-js";
 
 // add this to env file
 const secret = "65MeaID67UmBlZ/Nb8RCOfUAF3gkoproJra9L+J9fmM="
-const customIV: cryptoJS.lib.WordArray = cryptoJS.enc.Utf8.parse("Iye16+LR+plUr/qJQaGrkw==")
+const customIV: cryptoJS.lib.WordArray = cryptoJS.enc.Base64.parse("Iye16+LR+plUr/qJQaGrkw==")
 
 export function aesEncrypt(message: string) {
     let ciphertext = cryptoJS.AES.encrypt(message, secret, { iv: customIV });
